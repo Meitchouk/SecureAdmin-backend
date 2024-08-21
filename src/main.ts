2c464535fbf101c78345e71bd6bc3c2e2d80a5cb
Marat Lanza <lanzamarat@gmail.com>
2024-08-21 13:35:15 -0600

    Migrated deployment from Vercel to Heroku and successfully deployed the application. Added redirect to Swagger documentation at the root path.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { PrismaService } from "./prisma/prisma.service";
-
+import { Response } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +24,18 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  // Middleware para redirigir desde la raíz a la documentación de Swagger
+  app.use('/', (req, res: Response) => {
+    res.send(`
+      <html>
+        <body>
+          <h1>Bienvenido a SecureAdmin API</h1>
+          <p>Accede a la <a href="/api/docs">documentación de Swagger</a>.</p>
+        </body>
+      </html>
+    `);
+  });
 
   // Servicio de Prisma
   const prismaService = app.get(PrismaService);
