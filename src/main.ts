21a47f0fc87627cf5e56f0f8be2b5b8bfe4e6cdb
Marat Lanza <lanzamarat@gmail.com>
2024-08-23 10:08:47 -0600

    CORS CHANGE

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
   // Habilitar CORS para localhost y un dominio futuro
   app.enableCors({
     origin: [
-      '*',
+      'http://localhost:5173',
+      'https://secure-admin-frontend-cd2f018cd39c.herokuapp.com',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
